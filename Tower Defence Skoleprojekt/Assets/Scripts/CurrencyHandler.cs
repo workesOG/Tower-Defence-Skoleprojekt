139c6a36cbdcb97174ab508a642918a1db7e98fd
VeryWet-Duck <149181322+VeryWet-Duck@users.noreply.github.com>
2024-09-13 12:25:55 +0200

    Money haha
    Money script, skal videreudvikles og så optimere "lives" display

--- Tower Defence Skoleprojekt/Assets/Scripts/CurrencyHandler.cs
+++ Tower Defence Skoleprojekt/Assets/Scripts/CurrencyHandler.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CurrencyHandler : MonoBehaviour
 {
     public static CurrencyHandler Instance { get; private set;}
 
-    public int Money; // Antal penger
-	System.Random random = new System.Random();
+    [SerializeField]
+	private int startingCapital;
 
+    private int money;
+    public int Money{
+        get => money;
+        set{
+            money = value;
+            moneyScreenText.text = $"Money: {money}";
+        }
+    }
+	System.Random random = new System.Random();
+    public TextMeshProUGUI moneyScreenText;
     void Awake()
     {
         Instance = this;
+        Money = startingCapital;
     }
   	public void GainMoney()
 	{
--- Tower Defence Skoleprojekt/Assets/Scripts/GameController.cs
+++ Tower Defence Skoleprojekt/Assets/Scripts/GameController.cs
@@ -7,20 +7,32 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
 	public static GameController Instance;			// Laver �t objekt ud af GameController-klassen kaldet Instance
-	public int lifes;                               // Antal liv
+	
+	[SerializeField]
+	private int startingLives;
+	
+	private int lives;                               // Antal liv
+	public int Lives{
+		get => lives;
+		set
+		{
+			lives = value;
+			lifesScreenText.text = $"Lifes: {value}";   // S�tter v�rdien af teksten p� sk�rmen til at vise liv
+		}
+	}
 	public TextMeshProUGUI lifesScreenText;			// Teksten p� sk�rmen
 
 	private void Awake()							// Awake er ligesom Start-metoden, den k�rer bare tidligere
 	{
 		Instance = this;							// Instance = denne klasse (GameController)
+		Lives = startingLives;
 	}
 
 	public void LoseLife()
 	{
-		lifes = lifes - 1;                          // Man kan ogs� skrive "lifes--", det giver det samme
-		lifesScreenText.text = $"Lifes: {lifes}";   // S�tter v�rdien af teksten p� sk�rmen til at vise liv
+		Lives -= 1;                          // Man kan ogs� skrive "lifes--", det giver det samme
 
-		if ( lifes < 1)
+		if (Lives < 1)
 		{
 			print("Game Lost!");
 			Time.timeScale = 0;						// S�tter tiden i st�. 1 = normaltid.
